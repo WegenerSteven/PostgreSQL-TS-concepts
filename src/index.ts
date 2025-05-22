9d4f910bd2f71642d9c321158022f1823a5133da
WegenerSteven <wegenersteven580@gmail.com>
2025-05-22 21:09:10 +0300

    query update

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { initializeTables } from "./config/librarydb";
 
-import { insertoneStudent, Students, insertMultipleStudents, query } from "./queries/students-query";
+import { /*insertoneStudent,*/ Students, insertMultipleStudents, query } from "./queries/students-query";
 
 // This is the main entry point of the application
 // It initializes the database and performs operations on it
@@ -11,21 +11,21 @@ import { insertoneStudent, Students, insertMultipleStudents, query } from "./que
         await initializeTables();
 
         //insert a student 
-        const studentId = await insertoneStudent({ Fname: 'Antony', Lname: 'Gichuki', email: 'antony@gmail.com' })
-        console.log(`inserted student with ID: ${studentId}`);
+        // const studentId = await insertoneStudent({ Fname: 'Antony', Lname: 'Gichuki', email: 'antony@gmail.com' })
+        // console.log(`inserted student with ID: ${studentId}`);
 
         //insert other students with a transaction
-        const studentsToInsert: Students[] = [
-            { Fname: 'Benard', Lname: 'Mugambi', email: 'benard@gmail.com' },
-            { Fname: 'Catherine', Lname: 'Wanjiru', email: 'catherine@gmail.com' },
-            { Fname: 'David', Lname: 'Karanja', email: 'david@gmail.com' },
-            { Fname: 'Eunice', Lname: 'Wambui', email: 'eunice@gmail.com' },
-            { Fname: 'Faith', Lname: 'Wairimu', email: 'faith@gmail.com' },
-            { Fname: 'George', Lname: 'Mwangi', email: 'george@gmail.com' },
-            { Fname: 'Hellen', Lname: 'Njeri', email: 'hellen@gmail.com' },
-            { Fname: 'Irene', Lname: 'Wanjiru', email: 'irene@gmail.com' },
-        ];
-        await insertMultipleStudents(studentsToInsert);
+        // const studentsToInsert: Students[] = [
+        //     { Fname: 'Benard', Lname: 'Mugambi', email: 'benard@gmail.com' },
+        //     { Fname: 'Catherine', Lname: 'Wanjiru', email: 'catherine@gmail.com' },
+        //     { Fname: 'David', Lname: 'Karanja', email: 'david@gmail.com' },
+        //     { Fname: 'Eunice', Lname: 'Wambui', email: 'eunice@gmail.com' },
+        //     { Fname: 'Faith', Lname: 'Wairimu', email: 'faith@gmail.com' },
+        //     { Fname: 'George', Lname: 'Mwangi', email: 'george@gmail.com' },
+        //     { Fname: 'Hellen', Lname: 'Njeri', email: 'hellen@gmail.com' },
+        //     { Fname: 'Irene', Lname: 'Wanjiru', email: 'irene@gmail.com' },
+        // ];
+        // await insertMultipleStudents(studentsToInsert);
 
         //query all student to verify
         const student = await query();
